8f10afc647beb2454821a4d379dace2a711d2918
bsoliveira <bruno.silva.oliveira.uba@gmail.com>
2015-08-08 22:47:54 +0000

    fase 3 migration fix

---
+++ app/Entities/ProjectMember.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace CodeProject\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class ProjectMember extends Model implements Transformable
+{
+    use TransformableTrait;
+    
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    		'project_id',
+    		'user_id',
+    	];
+}
