41321c561d8b99063d2c107e9e9733e64add383f
bsoliveira <bruno.silva.oliveira.uba@gmail.com>
2015-08-23 15:38:30 +0000

    Frontend Projeto Fase 1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Laravel</title>
 
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	@if(Config::get('app.debug'))
+		<link rel="stylesheet" href="{{ asset('build/css/vendor/bootstrap.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('build/css/vendor/bootstrap-theme.min.css') }}">
+	@else
+		<link rel="stylesheet" href="{{ elixir('css/all.css') }}">
+	@endif
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -56,7 +61,17 @@
 	@yield('content')
 
 	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	@if(Config::get("app.debug"))
+		<script src="{{ asset('build/js/vendor/jquery.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/angular.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/angular-route.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/angular-resource.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/angular-animate.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/angular-messages.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/ui-bootstrap.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/navbar.min.js') }}"></script>
+	@else
+		<script src="{{ elixir('js/all.js') }}"></script>
+	@endif
 </body>
 </html>
